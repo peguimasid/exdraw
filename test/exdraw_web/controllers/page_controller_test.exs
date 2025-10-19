eaf3cc063ccd08eabfd75112e0e4036394d68406
Guilhermo Masid <guilhermomasid@gmail.com>
2025-10-18 22:43:53 -0300

    feat: simple landing page, lets not complicate things

--- test/exdraw_web/controllers/page_controller_test.exs
+++ test/exdraw_web/controllers/page_controller_test.exs
@@ -3,6 +3,7 @@ defmodule ExdrawWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "This is the main page"
+    assert html_response(conn, 200) =~ "Draw together"
+    assert html_response(conn, 200) =~ "in real-time"
   end
 end
