509a2a1ccb6fa8ad6381f3e54b4900e50c44ae2e
Guilhermo Masid <guilhermomasid@gmail.com>
2025-10-21 19:25:24 -0300

    feat: create drawing canvas

--- lib/exdraw_web/live/home_live/index.ex
+++ lib/exdraw_web/live/home_live/index.ex
@@ -7,19 +7,20 @@ defmodule ExdrawWeb.HomeLive.Index do
     <div class="h-dvh w-full flex flex-col">
       <Layouts.flash_group flash={@flash} />
       <Layouts.header current_scope={@current_scope} />
-      <div>This is the page to have the board</div>
-      <button phx-click="add" class="btn btn-primary size-fit">{@count}</button>
+      <div class="flex-1 w-full p-10 overflow-hidden">
+        <canvas
+          id="drawing-canvas"
+          phx-hook="DrawingCanvasHook"
+          phx-update="ignore"
+          class="border w-full h-full block"
+        />
+      </div>
     </div>
     """
   end
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, count: 0)}
-  end
-
-  @impl true
-  def handle_event("add", _params, socket) do
-    {:noreply, assign(socket, count: socket.assigns.count + 1)}
+    {:ok, socket}
   end
 end
