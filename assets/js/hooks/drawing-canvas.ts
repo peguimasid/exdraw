64d1cf2866e651ed9c9fb7cb3229726aec1d1e38
Guilhermo Masid <guilhermomasid@gmail.com>
2025-10-22 19:47:37 -0300

    ai generated, lets see how it behaves in prod

--- assets/js/hooks/drawing-canvas.ts
+++ assets/js/hooks/drawing-canvas.ts
@@ -3,6 +3,9 @@ import { Hook, makeHook } from 'phoenix_typed_hook'
 class DrawingCanvasHook extends Hook {
   private canvas!: HTMLCanvasElement
   private ctx!: CanvasRenderingContext2D
+  private isDrawing: boolean = false
+  private lastX: number = 0
+  private lastY: number = 0
 
   mounted() {
     this.canvas = this.el as HTMLCanvasElement
@@ -11,13 +14,68 @@ class DrawingCanvasHook extends Hook {
     this.canvas.width = this.canvas.offsetWidth
     this.canvas.height = this.canvas.offsetHeight
 
-    // Listen for canvas clicks
-    this.canvas.addEventListener('click', (event) => {
-      const offsetX = event.offsetX
-      const offsetY = event.offsetY
+    // Set drawing style
+    this.ctx.strokeStyle = 'black'
+    this.ctx.lineWidth = 2
+    this.ctx.lineCap = 'round'
+    this.ctx.lineJoin = 'round'
 
-      // Send click event to the server (will be broadcasted to all users)
-      this.pushEvent('canvas_click', { x: offsetX, y: offsetY })
+    // Mouse down - start drawing
+    this.canvas.addEventListener('mousedown', (event) => {
+      this.isDrawing = true
+      const { offsetX, offsetY } = event
+      this.lastX = offsetX
+      this.lastY = offsetY
+
+      // Send mouse down event to the server
+      this.pushEvent('mouse_down', { x: offsetX, y: offsetY })
+    })
+
+    // Mouse move - draw if mouse is down
+    this.canvas.addEventListener('mousemove', (event) => {
+      if (!this.isDrawing) return
+
+      const { offsetX, offsetY } = event
+
+      // Send mouse move event to the server
+      this.pushEvent('mouse_move', { 
+        x: offsetX, 
+        y: offsetY,
+        lastX: this.lastX,
+        lastY: this.lastY
+      })
+
+      this.lastX = offsetX
+      this.lastY = offsetY
+    })
+
+    // Mouse up - stop drawing
+    this.canvas.addEventListener('mouseup', (event) => {
+      if (!this.isDrawing) return
+      
+      this.isDrawing = false
+      const { offsetX, offsetY } = event
+
+      // Send mouse up event to the server
+      this.pushEvent('mouse_up', { x: offsetX, y: offsetY })
+    })
+
+    // Mouse leave - stop drawing if mouse leaves canvas
+    this.canvas.addEventListener('mouseleave', () => {
+      if (this.isDrawing) {
+        this.isDrawing = false
+        this.pushEvent('mouse_up', { x: this.lastX, y: this.lastY })
+      }
+    })
+
+    // Listen for draw events from the server
+    this.handleEvent('draw_line', (data: { 
+      x: number
+      y: number
+      lastX: number
+      lastY: number
+    }) => {
+      this.drawLine(data.lastX, data.lastY, data.x, data.y)
     })
 
     this.handleEvent('draw_dot', (data: { x: number; y: number }) => {
@@ -25,10 +83,17 @@ class DrawingCanvasHook extends Hook {
     })
   }
 
+  drawLine(x1: number, y1: number, x2: number, y2: number) {
+    this.ctx.beginPath()
+    this.ctx.moveTo(x1, y1)
+    this.ctx.lineTo(x2, y2)
+    this.ctx.stroke()
+  }
+
   drawDot(x: number, y: number) {
     this.ctx.fillStyle = 'black'
     this.ctx.beginPath()
-    this.ctx.arc(x, y, 5, 0, Math.PI * 2)
+    this.ctx.arc(x, y, 1, 0, Math.PI * 2)
     this.ctx.fill()
   }
 }
--- lib/exdraw_web/live/home_live/index.ex
+++ lib/exdraw_web/live/home_live/index.ex
@@ -6,16 +6,18 @@ defmodule ExdrawWeb.HomeLive.Index do
   @impl true
   def render(assigns) do
     ~H"""
-    <Layouts.flash_group flash={@flash} />
-    <Layouts.header current_scope={@current_scope} />
-    <div class="flex-1 w-full p-10 overflow-hidden">
-      <canvas
-        id="drawing-canvas"
-        phx-hook="DrawingCanvasHook"
-        phx-update="ignore"
-        class="border w-full h-full block"
-      />
-    </div>
+    <main class="flex h-dvh flex-col size-full">
+      <Layouts.flash_group flash={@flash} />
+      <Layouts.header current_scope={@current_scope} />
+      <div class="size-full p-10">
+        <canvas
+          id="drawing-canvas"
+          phx-hook="DrawingCanvasHook"
+          phx-update="ignore"
+          class="border size-full rounded-lg"
+        />
+      </div>
+    </main>
     """
   end
 
@@ -28,20 +30,65 @@ defmodule ExdrawWeb.HomeLive.Index do
   end
 
   @impl true
-  def handle_event("canvas_click", %{"x" => x, "y" => y}, socket) do
-    # Broadcast the click event to all subscribers (including this user)
+  def handle_event("mouse_down", %{"x" => x, "y" => y}, socket) do
+    # Broadcast the mouse down event to all subscribers
     Phoenix.PubSub.broadcast(
       Exdraw.PubSub,
       @topic,
-      {:canvas_click, %{"x" => x, "y" => y}}
+      {:mouse_down, %{"x" => x, "y" => y}}
     )
 
     {:noreply, socket}
   end
 
   @impl true
-  def handle_info({:canvas_click, %{"x" => x, "y" => y}}, socket) do
-    # Send a message to the client to draw the dot via the hook
+  def handle_event(
+        "mouse_move",
+        %{"x" => x, "y" => y, "lastX" => last_x, "lastY" => last_y},
+        socket
+      ) do
+    # Broadcast the mouse move event to all subscribers
+    Phoenix.PubSub.broadcast(
+      Exdraw.PubSub,
+      @topic,
+      {:mouse_move, %{"x" => x, "y" => y, "lastX" => last_x, "lastY" => last_y}}
+    )
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("mouse_up", %{"x" => x, "y" => y}, socket) do
+    # Broadcast the mouse up event to all subscribers
+    Phoenix.PubSub.broadcast(
+      Exdraw.PubSub,
+      @topic,
+      {:mouse_up, %{"x" => x, "y" => y}}
+    )
+
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_info({:mouse_down, %{"x" => x, "y" => y}}, socket) do
+    # Draw a dot at the starting position
     {:noreply, push_event(socket, "draw_dot", %{"x" => x, "y" => y})}
+  end
+
+  @impl true
+  def handle_info(
+        {:mouse_move, %{"x" => x, "y" => y, "lastX" => last_x, "lastY" => last_y}},
+        socket
+      ) do
+    # Draw a line from the last position to the current position
+    {:noreply,
+     push_event(socket, "draw_line", %{"x" => x, "y" => y, "lastX" => last_x, "lastY" => last_y})}
+  end
+
+  @impl true
+  def handle_info({:mouse_up, _params}, socket) do
+    # Currently, we don't need to do anything on mouse up
+    # But we keep this handler in case we want to add functionality later
+    {:noreply, socket}
   end
 end
